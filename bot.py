d0332f6ee98e37b95b419c565797c6a702ad6375
Mustafa Kamran <mmkthecoolest@gmail.com>
2022-02-04 18:24:16 -0500

    The bot can now run on multiple servers

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import asyncio
 import imp
 import json
 import requests
@@ -6,44 +7,45 @@ import os
 import time
 from datetime import date, timedelta
 from discord.ext import tasks
+import threading
+
+print("test!")
 
 
-client = discord.Client()
-#channel_id_exists = False
-#channel_id = 0
+class MyClient(discord.Client):
+    channel_id = []
+    channel_id_exists = False
+
+    async def on_ready(self):
+        print('We have logged in as {0.user}'.format(self))
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content.startswith('!wordle_setup'):
+            await message.delete()
+            await message.channel.send("Setting up...")
+            self.channel_id.append(message.channel.id)
+            self.channel_id_exists = True
+
+    @tasks.loop(seconds=3600*24)
+    async def wordle_guess(self):
+        if self.channel_id_exists:
+
+            yesterday = date.today() - timedelta(days=1)
+
+            year = yesterday.year
+            month = yesterday.month
+            day = yesterday.day
+
+            for channel_id_iter in self.channel_id:
+                message_channel = self.get_channel(channel_id_iter)
+                word_guess = json.loads(requests.get(
+                    "https://najemi.cz/wordle_answers/api/?day={0}&month={1}&year={2}".format(day, month, year)).text)["word"]
+                await message_channel.send("Wordle guess for " + yesterday.isoformat() + " : " + word_guess + '\nhttps://www.merriam-webster.com/dictionary/' + word_guess)
 
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!wordle_setup'):
-        await message.delete()
-        await message.channel.send("Setting up...")
-        channel_id = message.channel.id
-        await wordle_guess(channel_id)
-
-
-async def wordle_guess(channel_id):
-
-    while True:
-        message_channel = client.get_channel(channel_id)
-        yesterday = date.today() - timedelta(days=1)
-
-        year = yesterday.year
-        month = yesterday.month
-        day = yesterday.day
-
-        word_guess = json.loads(requests.get(
-            "https://najemi.cz/wordle_answers/api/?day={0}&month={1}&year={2}".format(day, month, year)).text)["word"]
-        await message_channel.send("Word guess for " + yesterday.isoformat() + " : " + word_guess + '\nhttps://www.merriam-webster.com/dictionary/' + word_guess)
-
-        time.sleep(3600*24)
-
+client = MyClient()
+client.wordle_guess.start()
 client.run("TOKEN")
